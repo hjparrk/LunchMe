5f23bbb874f627363a20875f6f79bd5c759138fe
hjparrk <hyojoon2372552@naver.com>
2022-07-18 18:44:47 +0900

    add Logout button on the HomeScreen

--- LunchMe/screens/HomeScreen.js
+++ LunchMe/screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext } from "react";
+import { SafeAreaView, Text } from "react-native";
+
+import { AuthContext } from "../store/auth-context";
+import IconButton from "../components/ui/IconButton";
 
 function HomeScreen() {
+  const authCtx = useContext(AuthContext);
+
+  function logoutHandler() {
+    authCtx.logout();
+  }
+
   return (
-    <View>
-      <Text className="text-red-500">Home Screen!!!!</Text>
-    </View>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text className="text-red-500 text-3xl italic tracking-widest">
+        Home Screen!!!!
+      </Text>
+      <IconButton
+        iconFamily="AntDesign"
+        name="logout"
+        size={36}
+        color="black"
+        onPress={logoutHandler}
+      />
+    </SafeAreaView>
   );
 }
 
