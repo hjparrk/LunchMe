cf8784f8e058ced96a97200d726f1073257c1049
hjparrk <hyojoon2372552@naver.com>
2022-07-24 17:21:19 +0900

    fix IconButton error

---
+++ LunchMe/components/ui/IconButton.js
@@ -0,0 +1,19 @@
+import { Pressable } from "react-native";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
+
+function IconButton({ iconFamily, name, size, color, onPress }) {
+  function iconFamilyHandler() {
+    switch (iconFamily) {
+      case "AntDesign":
+        return <AntDesign name={name} size={size} color={color} />;
+      case "MaterialIcons":
+        return <MaterialIcons name={name} size={size} color={color} />;
+    }
+  }
+
+  const icon = iconFamilyHandler();
+
+  return <Pressable onPress={onPress}>{icon}</Pressable>;
+}
+
+export default IconButton;
